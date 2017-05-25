29b4cf605046ee1cdde686f2f73432ae30d95a09
Mayor2k <Maxim27r@outlook.com>
2017-05-25 21:55:51 +0300

    path

---
+++ _site/js/script.js
@@ -0,0 +1,42 @@
+//swipe patch
+$(document).ready(function(){
+	$(".carousel").swipe( {
+		swipeLeft: function() {
+			$(this).carousel("next");
+		},
+		swipeRight: function() {
+			$(this).carousel("prev");
+		},
+		allowPageScroll: "vertical"
+	});
+});
+//carousel timer
+$(document).ready(function(){
+	$('.carousel').carousel({
+  interval: false
+  })
+})
+//change with carousel
+$(document).ready(function(){
+	$("#carousel").on("slid.bs.carousel",function(event){
+		var count = $("#header div.active").index()
+		switch (count) {
+			case 0:
+				$(".nav-tabs a:eq(0)").tab("show")
+				$(".")
+				break;
+			case 1:
+				$(".nav-tabs a:eq(1)").tab("show")
+				break;
+			case 2:
+				$(".nav-tabs a:eq(2)").tab("show")
+				break;
+			case 3:
+				$(".nav-tabs a:eq(3)").tab("show")
+				break;
+			default:
+			  console.log("Error, index not found")
+		}
+	})
+})
+//description to images
