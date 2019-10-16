7d91ebfff42b68450dae511a88da966c013a888d
Maxim <Maxim27r@outlook.com>
2019-10-16 13:30:10 +0300

    add new web page

---
+++ zdorovey/script.js
@@ -0,0 +1,8 @@
+//set .header-description width equal to navbar width
+
+
+$(window).resize(function() {
+    $(".header-description").width($(".container").width());
+});
+
+$(window).trigger('resize');
