e5071871b8cbce9a310ebd2cf57563f506cfddf8
Maxim <Maxim27r@outlook.com>
2019-10-18 14:32:46 +0300

    work with other stuff

--- zdorovey/script.js
+++ zdorovey/script.js
@@ -1,8 +1,7 @@
 //set .header-description width equal to navbar width
-
-
 $(window).resize(function() {
     $(".header-description").width($(".container").width());
 });
 
 $(window).trigger('resize');
+//work with nav-pills
