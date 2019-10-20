d8b3a3a55457a992d84d99e193774ae670856a97
Maxim <Maxim27r@outlook.com>
2019-10-20 21:20:36 +0300

    add new content

--- zdorovey/script.js
+++ zdorovey/script.js
@@ -1,7 +1,23 @@
 //set .header-description width equal to navbar width
 $(window).resize(function() {
     $(".header-description").width($(".container").width());
+    //set maxHeight for images
+    length = $(".h-100").children().length;
+    //alert(length);
+    maxHeight = 0;
+    for(i=0;i<length;i++){
+      a = $(".h-100").children().eq(i).height();
+      if (i==0) {
+        maxHeight = a;
+      }else if (i!=0 && maxHeight>a && a!=0) {
+        maxHeight = a;
+      }else {
+        continue;
+      }
+    }
+    $(".h-100 img").css({"max-height":maxHeight});
+    //alert(maxHeight);
+
 });
 
 $(window).trigger('resize');
-//work with nav-pills
