a95a2a5b0973cf18741e33a635ed0dba51aea35b
Maxim <Maxim27r@outlook.com>
2019-10-25 16:30:43 +0300

    modal

--- zdorovey/script.js
+++ zdorovey/script.js
@@ -48,3 +48,21 @@ function topFunction() {
 window.onscroll = function() {
   scrollFunction()
 };
+//datapicker
+!function(a){a.fn.datepicker.dates.ru={days:["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"],daysShort:["Вск","Пнд","Втр","Срд","Чтв","Птн","Суб"],daysMin:["Вс","Пн","Вт","Ср","Чт","Пт","Сб"],months:["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],monthsShort:["Янв","Фев","Мар","Апр","Май","Июн","Июл","Авг","Сен","Окт","Ноя","Дек"],today:"Сегодня",clear:"Очистить",format:"dd.mm.yyyy",weekStart:1,monthsTitle:"Мсяцы"}}(jQuery);
+
+$('#validationCustom04').datepicker({
+    uiLibrary: 'bootstrap4',
+    isRTL: false,
+    format: 'dd.mm.yyyy',
+    autoclose: true,
+    language: 'ru'
+});
+
+$('#validationCustom05').datepicker({
+    uiLibrary: 'bootstrap4',
+    isRTL: false,
+    format: 'dd.mm.yyyy',
+    autoclose: true,
+    language: 'ru'
+});
